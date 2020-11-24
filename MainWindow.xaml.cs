d856af0002bbdf8420c216181ba305f83c4fd550
chris vergeer <C.H.M.Vergeer@student.tudelft.nl>
2020-11-24 15:57:23 +0100

    first les BROKEN

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,9 +19,8 @@ namespace Dierentuin
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    { 
-
-
+    {
+        List<animal> animals = new List<animal>();
         public MainWindow()
         {
             InitializeComponent();
@@ -30,9 +29,73 @@ namespace Dierentuin
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            Animal_View.ItemsSource = ;
+            Animal_View.ItemsSource = animals.ToList();
         }
 
+        void Animal_Add(object sender, RoutedEventArgs e)
+        { 
+            if(Animal_Kind.Text == "Monkey")
+            {
+                animal animal = new Monkey
+                {
+                    name = Animal_Name.Text,
+                    energy = 100
+                };
+                animals.Add(animal);
+            }
+            else if(Animal_Kind.Text == "Lion")
+            {
+                animal animal = new Lion
+                {
+                    name = Animal_Name.Text,
+                    energy = 100
+                };
+                animals.Add(animal);
+            }
+            else
+            {
+                animal animal = new Elephant
+                {
+                    name = Animal_Name.Text,
+                    energy = 100
+                };
+                animals.Add(animal);
+            }            
+            Animal_View.ItemsSource = animals.ToList();
+        }
+
+        void Feed_All(object sender, RoutedEventArgs e)
+        {
+            foreach a in animals{
+
+            }
+        }
+
+
+        public class animal
+        {
+            public string name {get; set;}
+
+            public int energy { get; set; }
+
+            public int Eat()
+            {
+                return 25;
+            }
+        }
+
+        public class Monkey : animal
+        {
+
+        }
+        public class Lion : animal
+        {
+
+        }
+        public class Elephant: animal
+        {
+
+        }
 
     }
 }
