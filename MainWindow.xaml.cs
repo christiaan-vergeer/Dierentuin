608b8af5268b1a5b13a9872272b950d8ff7ec613
chris vergeer <C.H.M.Vergeer@student.tudelft.nl>
2020-11-26 11:15:11 +0100

    finished aplication

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Dierentuin
 {
@@ -24,12 +25,16 @@ namespace Dierentuin
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(0.5);
+            timer.Tick += Tick;
+            timer.Start();
             Loaded += MainWindow_Loaded;
         }
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            Animal_View.ItemsSource = animals.ToList();
+            update();
         }
 
         void Animal_Add(object sender, RoutedEventArgs e)
@@ -39,7 +44,7 @@ namespace Dierentuin
                 animal animal = new Monkey
                 {
                     name = Animal_Name.Text,
-                    energy = 100
+                    energy = 60
                 };
                 animals.Add(animal);
             }
@@ -60,17 +65,59 @@ namespace Dierentuin
                     energy = 100
                 };
                 animals.Add(animal);
-            }            
-            Animal_View.ItemsSource = animals.ToList();
+            }
+            update();
         }
 
         void Feed_All(object sender, RoutedEventArgs e)
         {
-            foreach a in animals{
-
+            Button btn = sender as Button;
+            foreach (var a in animals)
+            {
+                if ((btn.Name == "M" || btn.Name == "A") && a.GetType() == typeof(Monkey))
+                {
+                    a.energy = a.Eat(2.5);
+                }
+                else if ((btn.Name == "L" || btn.Name == "A") && a.GetType() == typeof(Lion))
+                {
+                    a.energy = a.Eat(1);
+                }
+                else if ((btn.Name == "E" || btn.Name == "A") && a.GetType() == typeof(Elephant))
+                {
+                    a.energy = a.Eat(0.5);
+                }
             }
+            update();
+        }        
+
+        void update()
+        {
+            Animal_View.ItemsSource = animals.ToList();
         }
 
+        void Tick(object sender, EventArgs e)
+        {
+            for (int i = animals.Count - 1; i >= 0; i--)
+            {
+                if (animals[i].GetType() == typeof(Monkey))
+                {
+                    animals[i].energy = animals[i].UseEnergy(5);
+                }
+                else if (animals[i].GetType() == typeof(Lion))
+                {
+                    animals[i].energy = animals[i].UseEnergy(1);
+                }
+                else
+                {
+                    animals[i].energy = animals[i].UseEnergy(2);
+                }
+                if (animals[i].energy < 0)
+                {
+                    animals.Remove(animals[i]);
+                }
+            }
+            update();
+        }
 
         public class animal
         {
@@ -78,9 +125,14 @@ namespace Dierentuin
 
             public int energy { get; set; }
 
-            public int Eat()
+            public int Eat(double devider)
             {
-                return 25;
+                return energy + (int)(25 / devider);
+            }
+
+            public int UseEnergy(double devider)
+            {
+                return energy - (int)(10 / devider);
             }
         }
 
